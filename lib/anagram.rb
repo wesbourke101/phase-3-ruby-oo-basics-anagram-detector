6e7d9cde47c9e8818f577d417cef7809b983ce14
wesley <wesbourkeavl@gmail.com>
2022-04-05 14:59:21 -0400

    commit tree 2518f678de20d4e3a95f3e71c37a98927a22f01d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram  
+    attr_accessor :name
+
+    def initialize(anagram)
+        @anagram=anagram
+    end  
+    
+    def match arr
+        arr.filter { |word| word.split('').sort == @anagram.split('').sort }
+    end    
+end    
